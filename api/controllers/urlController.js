cd74c7f93f1bc204bbeaf19ee13c90ef55c3e788
buisonanh <buisonanh.study@gmail.com>
2024-08-12 13:43:10 +0700

    ok

--- api/controllers/urlController.js
+++ api/controllers/urlController.js
@@ -8,7 +8,7 @@ exports.create_short_url = async (req, res) => {
     if (alias) {
         const existingUrl = await URL.findOne({ short_url: alias });
         if (existingUrl) {
-            return res.status(401).json({"error": "Alias alredy exists"});
+            return res.status(400).json({"error": "Alias alredy exists"});
         } else if (alias.length < 5) {
             return res.status(400).json({"error":"Alias should be at least 5 characters long"});
         } else { 
